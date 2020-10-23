704421ca264a5e006b86c4301ddf1ae7be2a9bd6
Viktor <krpectv57@gmail.com>
2020-10-23 20:26:47 +0200

    _

--- Open-Lab-04.02/Doubler.cs
+++ Open-Lab-04.02/Doubler.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Open_Lab_04._02
 {
@@ -6,7 +8,10 @@ namespace Open_Lab_04._02
     {
         public string DoubleChar(string original)
         {
-            throw new NotImplementedException();
+            string odpoved = "";
+            original.ToCharArray().ToList().ForEach(prop => odpoved += prop + prop.ToString());
+            return odpoved;
         }
+        
     }
 }
